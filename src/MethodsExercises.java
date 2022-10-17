449b10b8acf04fb77f721576b1d5f18cdbb67e5d
Nazareth Briseno <nazarethbriseno@gmail.com>
2022-10-17 10:26:24 -0500

    Updated Would you like to continue to the factorial

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,19 +1,19 @@
+
 import java.util.Scanner;
 
 public class MethodsExercises {
     public static void main (String[] args){
         Scanner myScanner = new Scanner(System.in);
         System.out.println("Enter Two Numbers: ");
-//        int number1 = myScanner.nextInt();
-//        int number2 = myScanner.nextInt();
-//        System.out.println("Those two numbers added together are " + addition(number1, number2));
-//        System.out.println("Your second number subtracted from your first number is: " + subtraction(number1, number2));
-//        System.out.println("Your numbers multiplied together are " + multiplication(number1, number2));
-//        System.out.println("Your first number divided by your second number is " + division(number1, number2));
-//        System.out.println("Your number divided by your second number will leave you with a remainder of: " + modulus(number1, number2));
-        int result = enterNumber();
-        System.out.println(result);
-        System.out.println(calculateFactorial(result));
+        int number1 = myScanner.nextInt();
+        int number2 = myScanner.nextInt();
+        System.out.println("Those two numbers added together are " + addition(number1, number2));
+        System.out.println("Your second number subtracted from your first number is: " + subtraction(number1, number2));
+        System.out.println("Your numbers multiplied together are " + multiplication(number1, number2));
+        System.out.println("Your first number divided by your second number is " + division(number1, number2));
+        System.out.println("Your number divided by your second number will leave you with a remainder of: " + modulus(number1, number2));
+        factorialQuestion();
+
 
     }
 
@@ -53,6 +53,27 @@ public class MethodsExercises {
             result = result * i;
         }
         return result;
+    }
+    public static String getFactorialSentence(int result){
+        String holder = result + "! = 1";
+        for(int i = 2; i <= result; i++){
+
+            holder += " X " + i;
+        }
+        return holder += " = " + calculateFactorial(result);
+    }
+    public static String factorialQuestion(){
+        String userChoice = "y";
+        while (userChoice.equals("y")){
+            System.out.println("Enter a number 1 through 10 to get the factorial: ");
+            Scanner scanner = new Scanner(System.in);
+            int userNumber = scanner.nextInt();
+            String factorialResults = getFactorialSentence(userNumber);
+            System.out.println(factorialResults);
+            System.out.println("Would you like to do another? y/n");
+            userChoice = scanner.next();
+        }
+            return "Goodbye";
     }
 
 
